6e0e5797b771a345208acb8c1a60d8411bb19ad9
ttorres93 <gp.torresrivas.antonio@gmail.com>
2026-08-19 17:19:37 -0600

    diseño

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
